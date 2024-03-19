90d62641a9b5a13de30d970d6771e37712d4365d
dadanhdyt <dadanhidyt@gmail.com>
2024-03-20 01:15:46 +0700

    feat:

---
+++ src/App.vue
@@ -0,0 +1,129 @@
+<script setup></script>
+
+<template>
+  <div class="sm:max-w-[960px] px-5 sm:px-0  container mx-auto">
+    <nav class="">
+      <div class="flex h-24 py-16 items-center justify-between">
+        <div class="text-3xl uppercase font-bold">Dadan HIdayat</div>
+        <ul class="flex text-lg items-center justify-between">
+          <li class="px-6 font-bold text-gray-800">
+            <a href="">Home</a>
+          </li>
+          <li class="px-6 font-bold text-gray-800">
+            <a href="">About</a>
+          </li>
+          <li class="px-6 font-bold text-gray-800">
+            <a href="">Blog</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="mt-5">
+      <h1 class="font-bold text-[1.5rem]">😃 Hi, I’m Dadan Hidayat!</h1>
+      <div class="flex flex-col gap-4 tracking-tight mt-4 text-lg">
+        <p>
+
+          I'm dedicated to assisting individuals (similar to yourself) in enhancing their skills, particularly in
+          contemporary career paths, web development, software engineering, the tech business sector, and overall
+          personal growth.
+        </p>
+        <p>
+          Student, Software Engineer, Full Stack Web Developer,
+          Community Leader, and Lifelong Learner.
+        </p>
+        <p>
+          Presently connected with:
+
+          🚀 SpaceTech Solutions
+          🔥 FireCode Innovations
+          🌟 StarDevelopers Collective
+          🎓 EduTech Academy
+          💡 BrightMinds Forum
+        </p>
+      </div>
+      <div class="my-6 flex items-start justify-start">
+        <img class="rounded-lg max-w-lg w-full" src="https://avatars.githubusercontent.com/u/149863272?v=4" alt="">
+      </div>
+
+      <div class="">
+        <p>
+          Fancy for some social media links and contents? Let’s connect on these platforms.
+
+        </p>
+        <div class="grid grid-cols-1 sm:grid-cols-2 my-6 gap-6 gap-y-5">
+          <div class="border px-4 py-2 rounded-xl">
+            <div class="flex items-center justify-start gap-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="80" viewBox="0 0 128 128">
+                <rect width="128" height="128" fill="none" />
+                <path fill="#0076b2"
+                  d="M116 3H12a8.91 8.91 0 0 0-9 8.8v104.42a8.91 8.91 0 0 0 9 8.78h104a8.93 8.93 0 0 0 9-8.81V11.77A8.93 8.93 0 0 0 116 3" />
+                <path fill="#fff"
+                  d="M21.06 48.73h18.11V107H21.06zm9.06-29a10.5 10.5 0 1 1-10.5 10.49a10.5 10.5 0 0 1 10.5-10.49m20.41 29h17.36v8h.24c2.42-4.58 8.32-9.41 17.13-9.41C103.6 47.28 107 59.35 107 75v32H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87 7.36-10.87 15V107H50.53z" />
+              </svg>
+              <div class="">
+                <ul>
+                  <li class="font-semibold">LinkedIn</li>
+                  <li>linkedin.com/in/dadanhdyt</li>
+                  <li class="text-gray-400">7000+ connections</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="border px-4 py-2 rounded-xl">
+            <div class="flex items-center justify-start gap-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 256 256">
+                <rect width="256" height="256" fill="none" />
+                <path fill="#1877f2"
+                  d="M256 128C256 57.308 198.692 0 128 0C57.308 0 0 57.308 0 128c0 63.888 46.808 116.843 108 126.445V165H75.5v-37H108V99.8c0-32.08 19.11-49.8 48.348-49.8C170.352 50 185 52.5 185 52.5V84h-16.14C152.959 84 148 93.867 148 103.99V128h35.5l-5.675 37H148v89.445c61.192-9.602 108-62.556 108-126.445" />
+                <path fill="#fff"
+                  d="m177.825 165l5.675-37H148v-24.01C148 93.866 152.959 84 168.86 84H185V52.5S170.352 50 156.347 50C127.11 50 108 67.72 108 99.8V128H75.5v37H108v89.445A128.959 128.959 0 0 0 128 256a128.9 128.9 0 0 0 20-1.555V165z" />
+              </svg>
+              <div class="">
+                <ul>
+                  <li class="font-semibold">Facebook</li>
+                  <li>linkedin.com/in/dadanhdyt</li>
+                  <li class="text-gray-400">7000+ connections</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="border px-4 py-2 rounded-xl">
+            <div class="flex items-center justify-start gap-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24">
+                <rect width="24" height="24" fill="none" />
+                <path fill="currentColor"
+                  d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2" />
+              </svg>
+              <div class="">
+                <ul>
+                  <li class="font-semibold">Github</li>
+                  <li>linkedin.com/in/dadanhdyt</li>
+                  <li class="text-gray-400">7000+ connections</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="border px-4 py-2 rounded-xl">
+            <div class="flex items-center justify-start gap-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="70" viewBox="0 0 256 180">
+                <rect width="256" height="180" fill="none" />
+                <path fill="#f00"
+                  d="M250.346 28.075A32.18 32.18 0 0 0 227.69 5.418C207.824 0 127.87 0 127.87 0S47.912.164 28.046 5.582A32.18 32.18 0 0 0 5.39 28.24c-6.009 35.298-8.34 89.084.165 122.97a32.18 32.18 0 0 0 22.656 22.657c19.866 5.418 99.822 5.418 99.822 5.418s79.955 0 99.82-5.418a32.18 32.18 0 0 0 22.657-22.657c6.338-35.348 8.291-89.1-.164-123.134" />
+                <path fill="#fff" d="m102.421 128.06l66.328-38.418l-66.328-38.418z" />
+              </svg>
+              <div class="">
+                <ul>
+                  <li class="font-semibold">Facebook</li>
+                  <li>linkedin.com/in/dadanhdyt</li>
+                  <li class="text-gray-400">7000+ connections</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
